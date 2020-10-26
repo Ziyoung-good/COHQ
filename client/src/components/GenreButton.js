72316e2b5eb888e56552a7bfd0538e1c1e8a3155
Ziyang Luo <ziyangluo@luoziyang.local>
2020-10-26 13:19:56 -0400

    all

---
+++ client/src/components/GenreButton.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default class GenreButton extends React.Component {
+	constructor(props) {
+		super(props);
+
+		
+	}
+
+	render() {
+		return (
+			<div className="genre" id={this.props.id} onClick={this.props.onClick}>
+			{this.props.genre}
+			</div>
+		);
+	}
+}
---
+++ server/db-config.js
@@ -0,0 +1,9 @@
+// Private. Will not be included in submission
+module.exports = {
+  host: "crazygame-db-instance.cuwzgtr2ojgq.us-east-2.rds.amazonaws.com",
+  user: "admin",
+  port: "3306",
+  password: "Lzy654321!",
+  database: "dbname",
+  multipleStatements: true
+};
