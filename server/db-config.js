88867e01cdc74eefc61bc20a470c938bdf02798d
Ziyoung-good <54610405+Ziyoung-good@users.noreply.github.com>
2022-05-10 15:23:50 -0400

    Update db-config.js

--- server/db-config.js
+++ server/db-config.js
@@ -1,9 +1,9 @@
 // Private. Will not be included in submission
 module.exports = {
-  host: "dbname2.cuwzgtr2ojgq.us-east-2.rds.amazonaws.com",
-  user: "admin",
-  port: "3306",
-  password: "Lzy654321!",
-  database: "dbname2",
+  host: "xxxx.rds.amazonaws.com",
+  user: "xxxx",
+  port: "xxx",
+  password: "xxxx",
+  database: "xxxx",
   multipleStatements: true
 };
